430ebeb67deddb7f4c03ff2cf7b5014eecf5f830
Kabir Bose <kabirbose04@gmail.com>
2025-02-13 11:35:01 -0500

    updated resume section

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -1,13 +1,15 @@
 export default function Resume() {
   return (
     <section id="resume" className="mb-12">
-      <h2 className="text-xl md:text-2xl font-bold mb-4">$ cat resume.pdf</h2>
-      <p className="mb-4">
+      <h2 className="text-2xl md:text-4xl mb-4">$ cat resume.pdf</h2>
+      <p className="mb-4 text-xl md:text-2xl">
         Here's a brief overview of my professional experience and skills:
       </p>
-      <ul className="list-disc list-inside mb-4">
-        <li>5+ years of experience in web development</li>
-        <li>Proficient in React, Next.js, TypeScript, and Node.js</li>
+      <ul className="list-disc list-inside mb-4 text-xl md:text-2xl">
+        <li>1 year of professional experience in a data centre environment</li>
+        <li>
+          Proficient in JavaScript, C++, Python, and respective frameworks
+        </li>
         <li>Strong understanding of responsive design and accessibility</li>
         <li>Experience with CI/CD pipelines and cloud platforms</li>
       </ul>
@@ -15,7 +17,7 @@ export default function Resume() {
         href="/path-to-your-resume.pdf"
         target="_blank"
         rel="noopener noreferrer"
-        className="inline-block border border-white p-2 invert-colors"
+        className="inline-block border border-white p-2 invert-colors text-xl md:text-2xl"
       >
         Download Full Resume
       </a>
