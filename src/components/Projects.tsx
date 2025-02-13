70ffdfd7a6b4f38d91b9493177a1d559a5c1324a
Kabir Bose <kabirbose04@gmail.com>
2025-02-13 11:23:28 -0500

    updated projects section

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -21,17 +21,17 @@ export default function Projects() {
 
   return (
     <section id="projects" className="mb-12">
-      <h2 className="text-xl md:text-2xl font-bold mb-4">$ ls projects/</h2>
+      <h2 className="text-2xl md:text-4xl mb-4">$ ls projects/</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {projects.map((project, index) => (
           <div key={index} className="border border-white p-4 invert-colors">
-            <h3 className="font-bold mb-2">{project.name}</h3>
-            <p className="mb-2">{project.description}</p>
+            <h3 className="text-xl md:text-2xl mb-2">{project.name}</h3>
+            <p className="text-lg md:text-xl mb-2">{project.description}</p>
             <div className="flex flex-wrap gap-2">
               {project.technologies.map((tech, techIndex) => (
                 <span
                   key={techIndex}
-                  className="text-xs bg-white text-black px-2 py-1 rounded"
+                  className="text-md md:text-lg bg-white text-black px-2 py-1 rounded"
                 >
                   {tech}
                 </span>
